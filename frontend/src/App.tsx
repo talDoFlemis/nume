d793042812a2730516bdaa762a3f819de22799c0
Said Rodrigues <coderflemis@gmail.com>
2025-04-01 16:02:23 -0300

    feat: add shadcn and tailwind

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import { Button } from "@/components/ui/button";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -25,7 +26,9 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <h1 className="text-3xl font-bold text-red-500 underline">
+        Hello world!
+      </h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -37,6 +40,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <Button>Click me</Button>
       <button onClick={fetchData}>Click to fetch from Go server</button>
       {message && (
         <div>
