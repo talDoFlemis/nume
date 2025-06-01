e9300b70ed99b202fb5b8d7b9e1d1146433dace8
Said Rodrigues <coderflemis@gmail.com>
2025-05-31 21:53:55 -0300

    refactor(tui): add new info

--- cmd/tui/main.go
+++ cmd/tui/main.go
@@ -1,19 +1,30 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
+	"os/user"
 
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
 	"github.com/taldoflemis/nume/internal/tui/models"
 )
 
 func main() {
 	// Start with the welcome screen
-	theme := models.ThemeCatppuccin()
-	// m := models.NewWelcomeModel(theme)
-	m := models.NewMainModel(theme)
-	
+	renderer := lipgloss.DefaultRenderer()
+	theme := models.ThemeCatppuccin(renderer)
+
+	currentUser, err := user.Current()
+	if err != nil {
+		fmt.Println("Error getting current user:", err)
+		return
+	}
+
+	m := models.NewWelcomeModel(theme, "TERM", renderer.ColorProfile().Name(), currentUser.Username)
+	// m := models.NewMainModel(theme)
+
 	p := tea.NewProgram(m, tea.WithAltScreen())
 	if _, err := p.Run(); err != nil {
 		log.Printf("Error running program: %v", err)
