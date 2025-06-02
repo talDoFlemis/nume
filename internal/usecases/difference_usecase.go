1a470566587c690a6da6da69a472faeefa34457b
Said Rodrigues <coderflemis@gmail.com>
2025-06-02 09:56:10 -0300

    feat(difference_usecase): add triple derivative with error order

--- internal/usecases/difference_usecase.go
+++ internal/usecases/difference_usecase.go
@@ -11,6 +11,15 @@ var (
 	ErrDeltaIsZero = errors.New("delta is zero")
 )
 
+type ErrorOrder uint8
+
+const (
+	LinearErrorOrder    ErrorOrder = 0
+	QuadraticErrorOrder ErrorOrder = 1
+	CubicErrorOrder     ErrorOrder = 2
+	QuarticErrorOrder   ErrorOrder = 3
+)
+
 type DifferenceStrategy interface {
 	Derivative(
 		ctx context.Context,
@@ -21,6 +30,12 @@ type DifferenceStrategy interface {
 		ctx context.Context,
 		simpleExpr expressions.SingleVariableExpr,
 		delta float64,
+	) (expressions.SingleVariableExpr, error)
+	TripleDerivative(
+		ctx context.Context,
+		simpleExpr expressions.SingleVariableExpr,
+		delta float64,
+		errorOrder ErrorOrder,
 	) (expressions.SingleVariableExpr, error)
 }
 
@@ -72,6 +87,34 @@ func (*ForwardDifferenceStrategy) DoubleDerivative(
 	}, nil
 }
 
+// TripleDerivative implements DifferenceStrategy.
+func (f *ForwardDifferenceStrategy) TripleDerivative(
+	ctx context.Context,
+	simpleExpr expressions.SingleVariableExpr,
+	delta float64,
+	errorOrder ErrorOrder,
+) (expressions.SingleVariableExpr, error) {
+	if delta == 0 {
+		return nil, ErrDeltaIsZero
+	}
+
+	fn := simpleExpr
+
+	switch errorOrder {
+	case LinearErrorOrder:
+		fn = func(variable float64) float64 {
+			numerator := simpleExpr(variable+3*delta) - 3*simpleExpr(variable+2*delta) +
+				+3*simpleExpr(variable+delta) - simpleExpr(variable)
+			denominator := delta * delta * delta
+			return numerator / denominator
+		}
+	default:
+		return nil, errors.New("unsupported error order for triple derivative in forward difference strategy")
+	}
+
+	return fn, nil
+}
+
 type BackwardDifferenceStrategy struct {
 }
 
@@ -109,6 +152,28 @@ func (*BackwardDifferenceStrategy) DoubleDerivative(
 		denominator := delta * delta
 		return numerator / denominator
 	}, nil
+}
+
+// TripleDerivative implements DifferenceStrategy.
+func (b *BackwardDifferenceStrategy) TripleDerivative(ctx context.Context, simpleExpr expressions.SingleVariableExpr, delta float64, errorOrder ErrorOrder) (expressions.SingleVariableExpr, error) {
+	if delta == 0 {
+		return nil, ErrDeltaIsZero
+	}
+	fn := simpleExpr
+
+	switch errorOrder {
+	case LinearErrorOrder:
+		fn = func(variable float64) float64 {
+			numerator := -simpleExpr(variable-3*delta) + 3*simpleExpr(variable-2*delta) +
+				-3*simpleExpr(variable-delta) + simpleExpr(variable)
+			denominator := delta * delta * delta
+			return numerator / denominator
+		}
+	default:
+		return nil, errors.New("unsupported error order for triple derivative in backward difference strategy")
+	}
+
+	return fn, nil
 }
 
 type CentralDifferenceStrategy struct {
@@ -149,4 +214,31 @@ func (*CentralDifferenceStrategy) DoubleDerivative(
 		denominator := delta * delta
 		return numerator / denominator
 	}, nil
+}
+
+// TripleDerivative implements DifferenceStrategy.
+func (c *CentralDifferenceStrategy) TripleDerivative(ctx context.Context,
+	simpleExpr expressions.SingleVariableExpr,
+	delta float64,
+	errorOrder ErrorOrder,
+) (expressions.SingleVariableExpr, error) {
+	if delta == 0 {
+		return nil, ErrDeltaIsZero
+	}
+
+	fn := simpleExpr
+
+	switch errorOrder {
+	case QuadraticErrorOrder:
+		fn = func(variable float64) float64 {
+			numerator := simpleExpr(variable+2*delta) - 2*simpleExpr(variable+delta) +
+				+2*simpleExpr(variable-delta) - simpleExpr(variable-2*delta)
+			denominator := delta * delta * delta * 2
+			return numerator / denominator
+		}
+	default:
+		return nil, errors.New("unsupported error order for triple derivative in central difference strategy")
+	}
+
+	return fn, nil
 }
